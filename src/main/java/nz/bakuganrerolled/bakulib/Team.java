f2ebf44a8e4c85795064eac81371f943199bb941
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-06 23:30:52 +1300

    Add Team related classes

---
+++ src/main/java/nz/bakuganrerolled/bakulib/Team.java
@@ -0,0 +1,39 @@
+package nz.bakuganrerolled.bakulib;
+
+import java.util.List;
+
+/**
+ * Represents a Team.
+ *
+ * @author Matt Eden
+ * @version 0.1
+ * @since 0.1
+ */
+public class Team {
+    List<Player> players;
+    TeamColour colour;
+
+    public Team(List<Player> players, TeamColour colour) {
+        this.players = players;
+        this.colour = colour;
+    }
+
+    /**
+     * Get players in the team.
+     *
+     * @return List of Players
+     * */
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * Get the colour of the team.
+     *
+     * @return Team Colour
+     * */
+    public TeamColour getColour() {
+        return colour;
+    }
+
+}
---
+++ src/main/java/nz/bakuganrerolled/bakulib/TeamColour.java
@@ -0,0 +1,12 @@
+package nz.bakuganrerolled.bakulib;
+
+/**
+ * Represents colours for teams.
+ *
+ * @author Matt Eden
+ * @version 0.1
+ * @since 0.1
+ * */
+public enum TeamColour {
+    RED, GREEN, BLUE, ORANGE, PURPLE, BROWN, YELLOW, PINK
+}
