3d0d4754dc57249efba1d56fc28a1a460c1d5bbd
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-07 00:23:41 +1300

    Create test for Team class

---
+++ src/test/java/nz/bakuganrerolled/bakulib/TeamTest.java
@@ -0,0 +1,37 @@
+package nz.bakuganrerolled.bakulib;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(LoggingExtension.class)
+public class TeamTest {
+    @Test
+    public void teamHasPlayers() {
+
+        Player dan = new Player("Dan", null);
+        Player shuji = new Player("Shuji", null);
+
+        Team team = new Team(new ArrayList<>() {{
+            add(dan);
+            add(shuji);
+        }}, TeamColour.RED);
+
+        assertEquals(new ArrayList<>() {{
+            add(dan);
+            add(shuji);
+        }}, team.getPlayers());
+    }
+
+    @Test
+    public void teamHasColour() {
+
+        Team team = new Team(null, TeamColour.GREEN);
+
+        assertEquals(TeamColour.GREEN, team.getColour());
+
+    }
+}
