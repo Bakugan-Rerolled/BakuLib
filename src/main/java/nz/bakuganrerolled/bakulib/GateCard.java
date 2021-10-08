b163dfb4db11209c3199899698337fe180ae43f7
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-09 00:05:41 +1300

    Add support for Bakugan on Gate Cards

--- src/main/java/nz/bakuganrerolled/bakulib/GateCard.java
+++ src/main/java/nz/bakuganrerolled/bakulib/GateCard.java
@@ -1,5 +1,8 @@
 package nz.bakuganrerolled.bakulib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents a Gate Card.
  *
@@ -9,13 +12,32 @@ package nz.bakuganrerolled.bakulib;
  */
 public class GateCard extends Card {
 
+    private final List<Bakugan> bakuganOnCard;
+
     public GateCard(String name, Effect effect) {
         this.name = name;
         this.effect = effect;
+        this.bakuganOnCard = new ArrayList<>();
     }
 
     @Override
     public void activate(Player owner, Context context) {
         effect.activate(owner, context);
+    }
+
+    /**
+     * Get all Bakugan on the Gate Card
+     *
+     * @return List of Bakugan on Card
+     * */
+    public List<Bakugan> getBakuganOnCard() {
+        return bakuganOnCard;
+    }
+
+    /**
+     * Places a Bakugan on the Gate Card
+     * */
+    public void placeBakuganOnCard(Bakugan b) {
+        bakuganOnCard.add(b);
     }
 }
