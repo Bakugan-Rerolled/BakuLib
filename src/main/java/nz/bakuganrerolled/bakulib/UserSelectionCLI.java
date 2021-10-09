fdd99e19823ea0f0a8bfa4637ded73b58866df5e
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-10 00:45:24 +1300

    Add 'exit early' support to selection CLI

--- src/main/java/nz/bakuganrerolled/bakulib/UserSelectionCLI.java
+++ src/main/java/nz/bakuganrerolled/bakulib/UserSelectionCLI.java
@@ -1,6 +1,7 @@
 package nz.bakuganrerolled.bakulib;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,26 +28,42 @@ public class UserSelectionCLI<T> extends UserSelection<T> {
 
         Scanner sc = new Scanner(System.in);
         List<T> selected = new ArrayList<>();
-        int remaining = this.maxSelection;
+        int remaining = (this.maxSelection == 0) ? options.size() : maxSelection;
         int index;
-        int selection;
+        String selection;
 
         while (remaining > 0) {
             index = 0;
             for (T option : options) {
                 System.out.println(++index + ". " + option.toString());
             }
-            /*
-             * if user enters Q or similar for selection
-             * check maxSelection - remaining against minSelection
-             * to ensure they have selected the minimum number of
-             * targets/options
-             * */
+
             System.out.print("Selection: ");
-            selection = sc.nextInt();
+            selection = sc.next();
+
+            /*
+             * If user enters Q or similar for selection
+             * check to ensure they have selected
+             * the minimum number of targets/options
+             * */
+            if (selection.equalsIgnoreCase("q")) {
+                if (selected.size() >= minSelection) {
+                    return selected;
+                }
+                else {
+                    System.out.printf("-- Must select %s more options%n", minSelection - selected.size());
+                }
+            }
+            else {
+                try {
+                    int n = Integer.parseInt(selection);
+                    selected.add(options.get(n - 1));
+                    remaining--;
+                } catch (NumberFormatException e) {
+                    System.out.println("-- Must input number");
+                }
+            }
             System.out.println(System.lineSeparator());
-            selected.add(options.get(selection - 1));
-            remaining--;
         }
 
         return selected;
