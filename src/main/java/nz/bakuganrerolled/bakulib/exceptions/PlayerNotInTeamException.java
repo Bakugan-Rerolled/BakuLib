10cfc560bb3f8933c7d31a3918c010a27d97a42f
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-12 00:37:46 +1300

    Add Javadoc comments to exceptions

--- src/main/java/nz/bakuganrerolled/bakulib/exceptions/PlayerNotInTeamException.java
+++ src/main/java/nz/bakuganrerolled/bakulib/exceptions/PlayerNotInTeamException.java
@@ -1,5 +1,13 @@
 package nz.bakuganrerolled.bakulib.exceptions;
 
+/**
+ * Represents when a Team attempts privileged
+ * interaction with a Player it does not contain.
+ *
+ * @author Matt Eden
+ * @version 0.1.0
+ * @since 0.1
+ * */
 public class PlayerNotInTeamException extends Exception {
     public PlayerNotInTeamException(String message) {
         super(message);
--- src/main/java/nz/bakuganrerolled/bakulib/exceptions/UnsatisfiedQueryException.java
+++ src/main/java/nz/bakuganrerolled/bakulib/exceptions/UnsatisfiedQueryException.java
@@ -1,5 +1,13 @@
 package nz.bakuganrerolled.bakulib.exceptions;
 
+/**
+ * Represents when an Effect attempts activation but
+ * the prerequisite Query is not satisfied.
+ *
+ * @author Matt Eden
+ * @version 0.1.0
+ * @since 0.1
+ * */
 public class UnsatisfiedQueryException extends Exception {
     public UnsatisfiedQueryException(String message) {
         super(message);
