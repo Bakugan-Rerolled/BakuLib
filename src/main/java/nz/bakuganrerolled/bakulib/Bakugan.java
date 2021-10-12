459023cb5314b83d47f3a461d8e5b9cef964c885
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-12 23:26:42 +1300

    Refactor Bakugan from class to interface
    Do for Bakugan what was done for Player, and for the same reasons.

--- src/main/java/nz/bakuganrerolled/bakulib/Bakugan.java
+++ src/main/java/nz/bakuganrerolled/bakulib/Bakugan.java
@@ -1,7 +1,5 @@
 package nz.bakuganrerolled.bakulib;
 
-import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.Set;
 
 /**
@@ -11,58 +9,50 @@ import java.util.Set;
  * @version 0.1.0
  * @since 0.1
  */
-public class Bakugan extends Item {
-    private final Set<Attribute> attributes;
-    private int gpower;
+public interface Bakugan {
 
-    public Bakugan(String name, int gpower, Attribute... attributes) {
-        this.name = name;
-        this.gpower = gpower;
-        this.attributes = EnumSet.copyOf(Arrays.asList(attributes));
-    }
+    /**
+     * Gets the Bakugan's name.
+     *
+     * @return Bakugan's name
+     */
+    String getName();
 
     /**
      * Gets the Bakugan's attribute(s).
      *
      * @return A set of attributes
      */
-    public Set<Attribute> getAttributes() {
-        return attributes;
-    }
+    Set<Attribute> getAttributes() ;
 
     /**
      * Adds an attribute to the Bakugan.
      */
-    public void addAttribute(Attribute attribute) {
-        attributes.add(attribute);
-    }
+    void addAttribute(Attribute attribute);
 
     /**
      * Removes an attribute from the Bakugan.
      */
-    public void removeAttribute(Attribute attribute) {
-        attributes.remove(attribute);
-    }
+    void removeAttribute(Attribute attribute);
 
     /**
      * Gets the Bakugan's GPower.
      *
      * @return Bakugan's GPower
      */
-    public int getGPower() {
-        return gpower;
-    }
+    int getGPower();
 
     /**
      * Changes the Bakugan's GPower.
      */
-    public void changeGPower(int gpower) {
-        this.gpower += gpower;
+    void changeGPower(int gpower);
+
+    default int getStamina() {
+        return 0;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s %s (%sG)", name, attributes.toString(), gpower);
+    default int getAgility() {
+        return 0;
     }
 
 }
